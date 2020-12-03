981d5b400de579ebbb2cfdb16a76d1c794adfc1b
Eric Xie <xiexieeric@gmail.com>
2020-12-02 20:17:58 -0500

    Incorporate isprime in static site

---
+++ assembly/index.ts
@@ -0,0 +1,19 @@
+// The entry file of your WebAssembly module.
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+
+export function isPrime(x: u32): bool {
+  if (x < 2) {
+      return false;
+  }
+
+  for (let i: u32 = 2; i < x; i++) {
+      if (x % i === 0) {
+          return false;
+      }
+  }
+
+  return true;
+}
