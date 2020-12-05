992ba9a6103b4b064212c5d5d7790520ae929465
Jordan Burton <jordieburton3@gmail.com>
2020-12-05 15:51:20 -0500

    console logs

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var webassembly = require('webassembly');
 async function loadWasm() {
     var module = await webassembly.load("math.wasm");
     window.wasmExports = module.exports;
+    console.log(window.wasmExports);
+    console.log(window.wasmExports.add(2, 2));
+    console.log(window.wasmExports.subtract(2, 2));
 }
 
 loadWasm();
